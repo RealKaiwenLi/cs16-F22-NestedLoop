402a457d8017363bc1dbe6801cb8f2b969c6feb7
RealKaiwenLi <asd903401772@gmail.com>
2022-12-02 22:10:33 -0800

    edit question description

--- tree.h
+++ tree.h
@@ -1,8 +1,7 @@
 /*
-Write a program that takes exactly one integer (height) parameter as argument and draws half of 
-the christmas tree. The program should print an error message if the input (height) is not divisible by 4.
-The height and the width of stem is 2 and it is excluded from the height of the tree. Note that there're no space after 
-the asterisk.
+Write a program that takes exactly one integer (width) parameter as argument and returns a string of half of 
+the christmas tree. The program should print an error message if the input (width) is less than 3 or is not even.
+Note that there're no space after the asterisk.
 Below are examples of the expected output when the program is executed on the unix bash shell with specific inputs.
 /printTree(3)
 *
